da5f2592b845792869d36ea0191f849ac6dec036
AnnaRozenbachs <57345254+AnnaRozenbachs@users.noreply.github.com>
2024-06-14 17:03:36 +0200

    updates

--- Program.cs
+++ Program.cs
@@ -13,15 +13,16 @@ StartApplication();
 
 void StartApplication()
 {
-    
-    
+    var taskDone = taskList.Where(t => t.Done).Count();
+    var taskNotDone = taskList.Where(t => !t.Done).Count();
+
+    helper.ConsoleWrite($"Welcome! You have {taskDone} tasks done and {taskNotDone} to do. Pick a option", ConsoleColor.White);
+
     while (true)
     {
-        var taskDone = taskList.Where(t => t.Done).Count();
-        var taskNotDone = taskList.Where(t => !t.Done).Count();
 
         //Input from user
-        var input = helper.ConsoleWrite($"Welcome! You have {taskDone} tasks done and {taskNotDone} to do. Pick a option\n" +
+        var input = helper.ConsoleWrite(
                         $"(1) Show Task List\n" +
                         $"(2) Add New Task\n" +
                         $"(3) Edit Task\n" +
@@ -37,13 +38,13 @@ void StartApplication()
                 case "2":
                     var task = helper.AddNewTask();
                     taskList.Add(task);
-                    input= helper.ConsoleWrite("Task is created. Press (4) for saving.", ConsoleColor.Green, true);
+                    helper.ConsoleWrite("Task is created. Press (4) for saving.", ConsoleColor.Green);
                     break;
                 case "3":
                     var id = helper.ConsoleWrite("Enter id of the item in list you want to edit", ConsoleColor.White, true);
                     var taskToEdit = taskList.Where(t => t.Id == int.Parse(id)).FirstOrDefault();
                     taskList = helper.EditTask(taskToEdit, taskList);
-                    input =  helper.ConsoleWrite("Task has been updated. Press (4) for saving.", ConsoleColor.Green, true);
+                    helper.ConsoleWrite("Task has been updated. Press (4) for saving.", ConsoleColor.Green);
                     break;
                 case "4":
                     helper.Save(taskList);
